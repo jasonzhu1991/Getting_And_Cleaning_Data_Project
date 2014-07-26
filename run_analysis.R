3b0e61010ebc4068f514e9b17e2372733c9bb6ee
Jason Zhu <jason@starmobileinc.com>
2014-07-26 17:27:53 -0400

    Deleted data directory from git and add downloading process into R script.

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,10 @@
-dir <- getwd()
+# If the data diretory not exist, download it, unzip it and delete the zip file.
+data_dir <- "UCI HAR Dataset"
+if (file.exists(data_dir)) {
+        url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+        zip_file <- "data.zip"
+        download.file(url, destfile = zip_file, method = "curl")
+        unzip(zip_file)
+        file.remove(zip_file)
+}
+
